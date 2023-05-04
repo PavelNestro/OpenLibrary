d8e6e935cf680ee8d8acbf10f6aca2acda948e22
Pavel <pavlik.simpson@gmail.com>
2023-05-04 18:41:42 +0300

    Fix desing and rating

--- OpenLibrary/Scenes/BooksScene/ViewModel/xib/TableViewCell.swift
+++ OpenLibrary/Scenes/BooksScene/ViewModel/xib/TableViewCell.swift
@@ -16,15 +16,33 @@ class TableViewCell: UITableViewCell {
     @IBOutlet weak var headerLabel: UILabel!
     @IBOutlet weak var imageViewCell: UIImageView!
     
+    @IBOutlet weak var imageHeart: UIImageView!
     static let identifier = "BooksTableViewCell"
 
     override func awakeFromNib() {
         super.awakeFromNib()
+    }
 
+    var imageDidTapHandler: (() -> ())?
+
+    var image: UIImage? = UIImage(systemName: "heart") {
+        didSet {
+            self.imageHeart.image = image
+        }
     }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
     }
 
+    private func setupTapGesture() {
+        let tapGesture = UITapGestureRecognizer()
+        tapGesture.numberOfTapsRequired = 1
+        tapGesture.addTarget(self, action: #selector(handleTap(_:)))
+        self.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc private func handleTap(_ sender: UITapGestureRecognizer)  {
+        imageDidTapHandler?()
+    }
     
 }
